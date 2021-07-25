90c65ac13acf356e376b501f98e6a40d45c27a62
Shawrat <sharathvenkateswaran@gmail.com>
2021-07-25 21:55:27 +0530

    ultrasonic sensor integration

--- servo.ino
+++ servo.ino
@@ -1,18 +1,59 @@
 #include <Servo.h>
 int pos=0;
 Servo mykek;
-void setup(){
-  mykek.attach(9);
+int trig = 3;
+int echo = 4;
+int time;
+float distance;
+float dist;
+
+float ultrasonicop(){
+  digitalWrite(trig, LOW);
+  digitalWrite(echo, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  time = pulseIn(echo,HIGH,30000);
+  distance = 343*0.0001*(time/2); // in centimeters
+  return distance;
+  delay(3000);
 }
 
+void servostop(){
+  mykek.write(90);
+  delay(100);
+}
+
+void servoforward(){
+  mykek.write(0);
+  delay(400);
+  mykek.write(180);
+  delay(400);
+  mykek.write(180);
+  delay(400);
+  mykek.write(0);  
+  delay(400);
+  // mykek.write(90);
+  // delay(400);
+}
+
+void setup(){
+    pinMode(trig,OUTPUT);
+    pinMode(echo,INPUT);
+    mykek.attach(9);
+    Serial.begin(9600);
+}
 
 void loop(){
-  for(pos=0; pos<=180; pos++){
-    mykek.write(pos);
-    delay(20);
-  }
-  for(pos=180; pos>=0; pos--){
-    mykek.write(pos);
-    delay(20);
-  }
+    dist = ultrasonicop();
+    Serial.println("Distance : ");
+    Serial.println(dist);
+    if (dist <= 30.0){
+      servostop();
+    }
+    else{
+      servoforward();
+    }
+
 }
